8517565db390745adaf8402061e106a6557fe79b
erlin gomez <kantuspro007@outlook.com.com>
2024-05-08 19:53:13 -0500

    first commit

--- project_final/urls.py
+++ project_final/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
-
+from django.contrib import admin
 # Create your views here.
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.LoginView.as_view(), name='login'),
+    path('logout/', views.vista_logout, name='logout'),
+    path('inicio/', views.inicio, name='inicio'),
     path('lo_mejor/', views.lo_mejor, name='lo_mejor'),
     path('historias_beats/', views.historias_beats, name='historias_beats'),
     path('registrar_apostadores/', views.registrar_apostadores, name='registrar_apostadores'),  
--- project_final/views.py
+++ project_final/views.py
@@ -2,22 +2,52 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import apostadores, Caracteristica_apostadores
 from .forms import FormRegistrarApostador, FormCaracteristicaApostadores
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout 
 
 
 
+
+class LoginView(LoginView):
+    template_name = 'login.html'
+    Authentication_Form = AuthenticationForm
+    
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["formulario"] = self .Authentication_Form()
+        return context
+    
+
+
+def vista_logout(request):
+    logout(request)
+    
+    return redirect('login')
+
+
+
+
+
+
+
+@login_required
 def inicio(request):
     return render(request, 'inicio.html')
 
-
+@login_required
 def lo_mejor(request):
     return render(request, 'lo_mejor.html')
 
 
-
+@login_required
 def historias_beats(request):
     return render(request, 'historias_beats.html')
 
-
+@login_required
 def registrar_apostadores(request):
     if request.method == 'POST':
         form = FormRegistrarApostador(request.POST)
@@ -33,7 +63,7 @@ def registrar_apostadores(request):
         form = FormRegistrarApostador()
     return render(request, 'registrar_apostador.html',{
         'formulario': form})
-
+@login_required
 def registrar_caracteristicas(request, apostador_id):
     apostador = get_object_or_404(apostadores, pk=apostador_id)
 
@@ -57,14 +87,14 @@ def registrar_caracteristicas(request, apostador_id):
 
 
 
-
+@login_required
 def lista_apostadores(request):
     registros_apostadores = apostadores.objects.all()
     return render (request, 'apostadores_lista.html', {
         'apostadores': registros_apostadores
     } )
 
-
+@login_required
 def info_apostador(request, id):
     apostador = apostadores.objects.get(id=id)
     return render(request, "info_apostadores.html", {
@@ -72,7 +102,7 @@ def info_apostador(request, id):
     })
 
 
-
+@login_required
 def eliminar_apostador(request, id):
     apostador = apostadores.objects.get(id=id)
     apostador.delete()
